743765eae7dcf3c59fa8f904ea2fe86179a1adb1
Rodrigo Cerqueira <rodrigocerqueira@outlook.com>
2023-03-24 22:26:57 -0300

    Tests passing

--- src/main/java/com/devsuperior/bds01/Bds01Application.java
+++ src/main/java/com/devsuperior/bds01/Bds01Application.java
@@ -2,7 +2,9 @@ package com.devsuperior.bds01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 @SpringBootApplication
 public class Bds01Application {
 
